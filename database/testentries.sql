935f47b988a57814c4c56f7a58e6261dfc90a7d0
Leandro Vivares <redlyoe@gmail.com>
2026-01-23 00:47:12 -0500

    Create testentries.sql
    commands to populate tables with test entries

---
+++ database/testentries.sql
@@ -0,0 +1,50 @@
+-- Users table test entries
+INSERT INTO Users (Username, Password, FirstName, LastName)
+VALUES ('KMPM6', 'isthis10charactersyet', 'King', 'Marlon');
+INSERT INTO Users (Username, Password, FirstName, LastName)
+VALUES ('Xx_ERNIELIAM_xX', '$specialcharacters$', 'Ernie', 'Liam');
+INSERT INTO Users (Username, Password, FirstName, LastName)
+VALUES ('rupert_jones2@yahoo.com', 'CAPITALS', 'Rupert', 'Jones');
+INSERT INTO Users (Username, Password, FirstName, LastName)
+VALUES ('username123', 'SomethingSomething123', 'John', 'Doe');
+INSERT INTO Users (Username, Password, FirstName, LastName)
+VALUES ('blackshadowflame', 'imsoevil', 'Bobbie', 'Small');
+INSERT INTO Users (Username, Password, FirstName, LastName)
+VALUES ('::j::', 'spthenqr', 'Juli', 'Siezer');
+INSERT INTO Users (Username, Password, FirstName, LastName)
+VALUES ('[[<==RIDICULOUSNAME==>]]^C', '__1234567890__', 'Xess', 'FSR4');
+INSERT INTO Users (Username, Password, FirstName, LastName)
+VALUES ('sumslightsumslight', 'dimsumbrightdifference', 'Cece', 'Plusplus');
+
+-- Contacts table test entries
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (1,'Plug','','idkman@gmail.com','555-555-5555');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (1,'Mr','President','thewhitehouse@wh.gov','001-001-0001');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (1,'Fortnite','Coach','getgood@gmail.com','133-133-1337');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (1,'Whatchamacallit','','','');
+
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (2,'Rupert','Jones Sr.','therj@aol.com','');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (2,'e','z','A','B');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (2,'1','2','3','4');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (2,'WAD','','55','+1 400-500-6000');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (2,'','','notanemail@e.com','1-800 MORGAN & MORGAN');
+
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (3,'Rupert','Jones Sr.','therj@aol.com','');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (3,'The','Police','police@email.com','111');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (3,'','','','');
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (3,'','','','');
+
+INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone)
+VALUES (4,'The','Wife','mylovelywife@gmail.com','999-999-9999');
