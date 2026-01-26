62b804c308568be9e18c28dfe064ce1726a6d87a
jcgallo <jo116371@ucf.edu>
2026-01-25 20:51:23 -0500

    Created log-in API, needs to be edited with the appropriate SQL database information.

---
+++ api/login.php
@@ -0,0 +1,60 @@
+<?php>
+    $inData = getRequestInfo();
+
+    // Fill in with exact SQL database specs
+    $host = "localhost:8000";
+    $db = "XXXXXXX";
+    $user = "root";
+    $pwd = "XXXXXX";
+
+    $ID = 0;
+    $firstName = "";
+    $lastName = "";
+
+    $conn = new mysqli($host, $user, $pwd, $db);
+ 
+    if($conn->connect_errno){
+        http_response_code(400);
+        header('Content-type: text/plain');
+        echo $conn->connect_error;
+        exit();
+    }
+    else {
+        $stmt = $conn->prepare("SELECT ID, FirstName, LastName FROM Users WHERE (Login=? AND Password=?)");
+        $stmt->bind_param("ss", $inData["login"], $inData["password"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($row = $result->fetch_assoc()){
+            returnWithInfo($row['FirstName'], $row['LastName'], $row['ID']);
+        }
+        else{
+            returnWithError("No records found");
+        }
+        $stmt->close();
+        $conn->close();
+    }
+
+    function getRequestInfo(){
+        $data = json_decode(file_get_contents('php://input'), true);
+        return $data;
+    }
+    
+    function sendResultInfoAsJson($obj){
+        header('Content-type: application/json');
+        echo $obj;
+    }   
+
+    function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
